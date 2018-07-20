ac0e82633bd6c508b8deee7033b91c1a1709aa19
Deep-Innovations <deep.innovations01@gmail.com>
2018-07-20 00:42:34 -0500

    d

---
+++ McKessonTestingFinal/src/main/java/com/mck/qa/pages/CareerTabPage.java
@@ -0,0 +1,11 @@
+package com.mck.qa.pages;
+
+public class CareerTabPage {
+	public CareerTabPage() {
+		
+	}
+
+	public void main (String [] args0) {
+		System.out.println("This is the Career's Tab, Welcome");
+	}
+}
